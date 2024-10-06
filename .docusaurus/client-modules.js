51a43de4f25a5c82c4f03bd75293989ebbe68bed
Mos6581p <116892660+ant6581@users.noreply.github.com>
2024-10-06 13:50:06 +0300

    Deploy website - based on cb18739bd4cc638e59bc5be5ab13d6a2d9d632d5

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/elias/projects/cyber4nt/cyber4nt.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/elias/projects/cyber4nt/cyber4nt.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/elias/projects/cyber4nt/cyber4nt.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/elias/projects/cyber4nt/cyber4nt.github.io/src/css/custom.css"),
-];
