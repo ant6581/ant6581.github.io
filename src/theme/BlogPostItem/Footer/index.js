09f0eff6d1470b3f12c2ed83bc5455145d8b5ec4
Mos6581p <116892660+ant6581@users.noreply.github.com>
2024-10-20 22:06:33 +0300

    custom

--- src/theme/BlogPostItem/Footer/index.js
+++ src/theme/BlogPostItem/Footer/index.js
@@ -1,76 +1,72 @@
-import React from 'react';
-import clsx from 'clsx';
-import {useBlogPost} from '@docusaurus/plugin-content-blog/client';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import EditMetaRow from '@theme/EditMetaRow';
-import TagsListInline from '@theme/TagsListInline';
-import ReadMoreLink from '@theme/BlogPostItem/Footer/ReadMoreLink';
+import React from "react";
+import clsx from "clsx";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+import EditThisPage from "@theme/EditThisPage";
+import TagsListInline from "@theme/TagsListInline";
+import ReadMoreLink from "@theme/BlogPostItem/Footer/ReadMoreLink";
+import styles from "./styles.module.css";
+
+import BlogEndBar from "@site/customizations/BlogEndBar";
+import BlogReactions from "@site/customizations/BlogReactions";
+
 export default function BlogPostItemFooter() {
-  const {metadata, isBlogPostPage} = useBlogPost();
-  const {
-    tags,
-    title,
-    editUrl,
-    hasTruncateMarker,
-    lastUpdatedBy,
-    lastUpdatedAt,
-  } = metadata;
+  const { metadata, isBlogPostPage } = useBlogPost();
+  const { tags, title, editUrl, hasTruncateMarker } = metadata;
   // A post is truncated if it's in the "list view" and it has a truncate marker
   const truncatedPost = !isBlogPostPage && hasTruncateMarker;
   const tagsExists = tags.length > 0;
   const renderFooter = tagsExists || truncatedPost || editUrl;
+
+  // Get reactions from blog metadata.
+  const reactions = Object.keys(metadata.frontMatter).includes("reactions")
+    ? metadata.frontMatter.reactions
+    : [];
+  const slug = metadata.frontMatter.slug;
+
   if (!renderFooter) {
     return null;
   }
-  // BlogPost footer - details view
-  if (isBlogPostPage) {
-    const canDisplayEditMetaRow = !!(editUrl || lastUpdatedAt || lastUpdatedBy);
-    return (
-      <footer className="docusaurus-mt-lg">
-        {tagsExists && (
-          <div
-            className={clsx(
-              'row',
-              'margin-top--sm',
-              ThemeClassNames.blog.blogFooterEditMetaRow,
-            )}>
-            <div className="col">
-              <TagsListInline tags={tags} />
-            </div>
-          </div>
+  return (
+    <div>
+      <footer
+        className={clsx(
+          "row docusaurus-mt-lg",
+          isBlogPostPage && styles.blogPostFooterDetailsFull
         )}
-        {canDisplayEditMetaRow && (
-          <EditMetaRow
-            className={clsx(
-              'margin-top--sm',
-              ThemeClassNames.blog.blogFooterEditMetaRow,
-            )}
-            editUrl={editUrl}
-            lastUpdatedAt={lastUpdatedAt}
-            lastUpdatedBy={lastUpdatedBy}
-          />
-        )}
-      </footer>
-    );
-  }
-  // BlogPost footer - list view
-  else {
-    return (
-      <footer className="row docusaurus-mt-lg">
+      >
         {tagsExists && (
-          <div className={clsx('col', {'col--9': truncatedPost})}>
+          <div className={clsx("col", { "col--9": truncatedPost })}>
             <TagsListInline tags={tags} />
           </div>
         )}
+
+        <BrowserOnly>
+          {() => (
+            <div className={clsx("col", { "col--9": truncatedPost })}>
+              <BlogReactions reactions={reactions} slug={slug} />
+            </div>
+          )}
+        </BrowserOnly>
+
+        {isBlogPostPage && editUrl && (
+          <div className="col margin-top--sm">
+            <EditThisPage editUrl={editUrl} />
+          </div>
+        )}
+
         {truncatedPost && (
           <div
-            className={clsx('col text--right', {
-              'col--3': tagsExists,
-            })}>
+            className={clsx("col text--right", {
+              "col--3": tagsExists,
+            })}
+          >
             <ReadMoreLink blogPostTitle={title} to={metadata.permalink} />
           </div>
         )}
       </footer>
-    );
-  }
+      {!isBlogPostPage && <BlogEndBar />}
+    </div>
+  );
 }
