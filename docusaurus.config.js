9e268c1ea9f854b72bfb179074a3a35073c10d48
Mos6581p <116892660+ant6581@users.noreply.github.com>
2024-10-20 21:59:10 +0300

    custom

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,12 +89,12 @@ const config = {
           src: 'img/logo.svg',
         },
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
+          // {
+          //   type: 'docSidebar',
+          //   sidebarId: 'tutorialSidebar',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
           {to: '/', label: 'Blog', position: 'left'},
           // {
           //   href: 'https://github.com/facebook/docusaurus',
